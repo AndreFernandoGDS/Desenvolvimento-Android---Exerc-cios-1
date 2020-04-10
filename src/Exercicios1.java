dad78adc112e899bf4e786c3a5a7774e836f6f5c
Andre <andre.engetronica@gmail.com>
2020-04-10 09:52:40 -0300

    c1 - Modificado .gitignore

---
+++ src/Exercicios1.java
@@ -0,0 +1,11 @@
+/*1. Escreva uma função privada chamada helloWorld que não possui retorno algum e não
+recebe nenhum parâmetro.*/
+
+public class Exercicios1 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
