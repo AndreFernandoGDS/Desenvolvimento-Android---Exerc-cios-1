8f4726b5dd26d56539d836638cdb71f1ad2370cf
Andre <andre.engetronica@gmail.com>
2020-04-10 09:57:05 -0300

    c2 - Exercício 1 concluído

--- src/Exercicios1.java
+++ src/Exercicios1.java
@@ -3,8 +3,16 @@ recebe nenhum parâmetro.*/
 
 public class Exercicios1 {
 
+	private void helloWorld() {
+		
+		System.out.println("Hello World!");
+	}
+	
 	public static void main(String[] args) {
 		
+		Exercicios1 exerc = new Exercicios1();
+		
+		exerc.helloWorld();
 		
 	}
 
